13bcf569b94f0fb43f43352a380de4f7ba15f995
Bruno Leandro <bruno.lbs100@gmail.com>
2022-08-05 21:23:14 -0300

    Esqueleto inicial do jogo

---
+++ joguinho.rb
@@ -0,0 +1,98 @@
+require "ruby2d"
+
+set background: "black"
+set title: "Snake Game with Ruby"
+set fps_cap: 20
+SQUARE_SIZE = 20
+SCREEN_WIDTH = Window.width / SQUARE_SIZE
+SCREEN_HEIGHT = Window.height / SQUARE_SIZE
+
+class Snake 
+    def initialize 
+        @start_positions = [[2,0], [2,1], [2,2], [2,3]]
+    end
+
+    def create 
+    end 
+
+    def move 
+    end
+
+    def grow
+    end
+
+    private 
+
+    def head 
+    end 
+
+    def coordinates 
+    end
+end
+
+class Game 
+    def initialize
+        @score = 0
+    end 
+
+    def record_hit
+        @score +=1
+    end
+
+    def snake_eat_food 
+    end
+
+    def finish_game
+    end
+end
+
+snake = Snake.new 
+game = Game.new
+
+#Snake and Game Actions
+update do 
+end
+
+#Keyboard keys 
+on :key_down do |events|
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+show
